da45915d5da3fc71501478d249676ab888c4117a
Dylan <dylan.vallee@hotmail.com>
2021-12-24 09:36:43 +0100

    `selectors.test.js`: Fixed one and added tests for the `filteredTodosListSelector`.

--- src/features/todos/store/selectors.test.js
+++ src/features/todos/store/selectors.test.js
@@ -1,4 +1,4 @@
-import { todosSelector, todosListSelector, filteredTodosListSelector } from "./selectors";
+import { filteredTodosListSelector, todosListSelector, todosSelector } from "./selectors";
 import { createTodo } from "../__test__/helpers";
 
 describe('test todos selectors', function () {
@@ -39,6 +39,22 @@ describe('test todos selectors', function () {
     expect(todosListSelector(initialState)).toEqual([todo])
   });
 
+  test("should return the entire todos list", () => {
+    expect.assertions(1)
+    const [todoNotDone, todoDone] = [createTodo("Active"), createTodo("Done", true)]
+    const initialState = {
+      todos: {
+        data: [todoNotDone, todoDone],
+        loading: false,
+        error: null,
+      },
+      auth: {
+        isLoggedIn: false,
+      }
+    }
+
+    expect(filteredTodosListSelector(initialState, "all")).toEqual([todoNotDone, todoDone])
+  });
   test("should return list with only the 'done' todos", () => {
     expect.assertions(2)
     const [todoNotDone, todoDone] = [createTodo("Hello"), createTodo("World", true)]
@@ -54,6 +70,23 @@ describe('test todos selectors', function () {
     }
 
     expect(filteredTodosListSelector(initialState, "done")).toEqual([todoDone])
-    expect(filteredTodosListSelector(initialState, "done")).not.toContainEqual([todoNotDone])
+    expect(filteredTodosListSelector(initialState, "done")).not.toContainEqual(todoNotDone)
+  });
+  test("should return list with only the 'active' (done => false) todos", () => {
+    expect.assertions(2)
+    const [todoActive, todoDone] = [createTodo("Hello"), createTodo("World", true)]
+    const initialState = {
+      todos: {
+        data: [todoActive, todoDone],
+        loading: false,
+        error: null,
+      },
+      auth: {
+        isLoggedIn: false,
+      }
+    }
+
+    expect(filteredTodosListSelector(initialState, "active")).toEqual([todoActive])
+    expect(filteredTodosListSelector(initialState, "active")).not.toContainEqual(todoDone)
   });
 });
